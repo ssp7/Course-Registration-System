9920eee1bb5da7ae833f7b1511c9396b6438f04d
Ryan <rsthomas@huskers.unl.edu>
2019-09-28 10:37:41 -0500

    Implemented the Writer class to parse course lists and object lists to maps and write them to the files

--- src/main/java/edu/unl/cse/csce361/course_registration/backend/Writer.java
+++ src/main/java/edu/unl/cse/csce361/course_registration/backend/Writer.java
@@ -1,20 +1,71 @@
 package edu.unl.cse.csce361.course_registration.backend;
 import edu.unl.cse.csce361.course_registration.backend.CSVReaderWriter.*;
+import edu.unl.cse.csce361.course_registration.backend.Reader.*;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 
 public class Writer {
 
-    public boolean writeStudent(){
-
-        return false;
-    }
-
-    public  boolean writeCourse(StorageCourse course){
+    public boolean writeStudent(ArrayList<StorageStudent> students, String filename){
         Set<Map<String, String>> data = null;
+        for(StorageStudent student : students){
+            Map<String,String> studentMap = null;
+            studentMap.put("Name",student.getName());
+            studentMap.put("Major",student.getMajor());
+            ArrayList<String> registerCourses = student.getRegisteredCoursesID();
+            int num = 1;
+            for(int i = 0; i < registerCourses.size(); i++){
+                studentMap.put("RegisteredCourse"+num,registerCourses.get(i));
+                num++;
+            }
+            ArrayList<String> completedCourses = student.getCompletedCoursesID();
+            num = 0;
+            for(int i = 0; i < completedCourses.size(); i++){
+                studentMap.put("CompletedCourse"+num,completedCourses.get(i));
+                num++;
+            }
+            data.add(studentMap);
+        }
 
-        boolean success = CSVReaderWriter.writeCSV("courses", data);
+        boolean success = CSVReaderWriter.writeCSV(filename, data);
         return success;
     }
+
+    public  boolean writeCourse(ArrayList<StorageCourse> courses, String filename){
+        Set<Map<String, String>> data = null;
+        for(StorageCourse course : courses){
+            Map<String,String> courseMap = null;
+
+            courseMap.put("CourseID", course.getCourseID());
+            courseMap.put("Section", course.getSection());
+            courseMap.put("CourseName", course.getCourseName());
+            courseMap.put("Room",course.getRoom());
+            courseMap.put("Days",course.getDays());
+            courseMap.put("StartTime",course.getStartTime());
+            courseMap.put("Semester",course.getSemester());
+            courseMap.put("NumberOfStudentsRegistered",""+course.getStudentsRegistered());
+            courseMap.put("NumberOfAvailableSeats",""+course.getAvailableSeats());
+            courseMap.put("URL",course.getURL());
+            ArrayList<String> prerequisites = course.getPrerequisiteCourseIDs();
+            if(prerequisites.size() >= 1){
+                courseMap.put("Prerequisite1",prerequisites.get(0));
+                if(prerequisites.size() >= 2){
+                    courseMap.put("Prerequisite2",prerequisites.get(1));
+                }
+            }
+            data.add(courseMap);
+        }
+
+        boolean success = CSVReaderWriter.writeCSV(filename, data);
+        return success;
+    }
+
+//    public static void main(String[]args) {
+
+//        Reader.studentReader());
+//        Reader.courseReader());
+//
+//    }
 }
