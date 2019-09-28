72828bb357dfd46aec71b0336188212f50b090c8
Ryan <rsthomas@huskers.unl.edu>
2019-09-28 12:36:23 -0500

    Implemented facade with the readers and writers for logic layer to use

--- src/main/java/edu/unl/cse/csce361/course_registration/backend/BackendFacade.java
+++ src/main/java/edu/unl/cse/csce361/course_registration/backend/BackendFacade.java
@@ -1,8 +1,26 @@
 package edu.unl.cse.csce361.course_registration.backend;
 
+import java.util.ArrayList;
+
 public class BackendFacade {
 
    private boolean nameCheck;
-   
-	
+
+   public static ArrayList<StorageStudent> readStudents(){
+      return Reader.studentReader("students.csv");
+   }
+
+   public static ArrayList<StorageCourse> readCourses(){
+      return Reader.courseReader("courses.csv");
+   }
+
+   public static boolean writeStudents(ArrayList<StorageStudent> students){
+      return Writer.studentWriter(students, "students.csv");
+   }
+
+   public static boolean writeCourses(ArrayList<StorageCourse> courses){
+      return Writer.courseWriter(courses, "courses.csv");
+   }
+
+
 }
