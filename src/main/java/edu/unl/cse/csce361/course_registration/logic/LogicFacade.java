5a82b9f5ac9d5e9395baaab408487586f247e11f
Ryan Thomas <rsthomas@huskers.unl.edu>
2019-09-30 20:33:30 +0000

    Made static functions to manage enrollment from the front end.

--- src/main/java/edu/unl/cse/csce361/course_registration/logic/LogicFacade.java
+++ src/main/java/edu/unl/cse/csce361/course_registration/logic/LogicFacade.java
@@ -1,5 +1,16 @@
 package edu.unl.cse.csce361.course_registration.logic;
 
 public class LogicFacade {
+    
+    public static void enrollStudentInCourse(String studentName, int maxEnrollment, String courseID, String section){
+        LogicStudent student = new LogicStudent(studentName, maxEnrollment);
+        student.registerForCourse(courseID, section);
+    }
 
+    public static void dropStudentFromCourse(String studentName, String courseID){
+        LogicStudent student = new LogicStudent(studentName, 8);
+        student.removeCourse(courseID);
+    }
+
+    
 }
